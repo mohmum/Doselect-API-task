b6cf2b9048aa858f9c965f1b17ab60f13efa0b03
Mohsin Mumtaz <mohsinmumtaz21@gmail.com>
2017-11-04 14:09:22 +0530

    added jpg lossless compression

--- image_store/views.py
+++ image_store/views.py
@@ -2,13 +2,9 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.core.files.storage import FileSystemStorage
-from django.shortcuts import render,redirect
 import os
-from .forms import Loginform
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
-from django.contrib.auth import authenticate,login,logout
-from django.views.generic import View
 from django.conf import settings
 
 class Oneimage(APIView):
@@ -32,12 +28,12 @@ class Oneimage(APIView):
         if img_name in path:
             os.chdir(os.path.join(settings.MEDIA_ROOT,user,'photos'))
             os.remove(img_name)
-            return Response(status=status.HTTP_204_NO_CONTENT)
+            return Response({'message': 'image successfully deleted'},status=status.HTTP_204_NO_CONTENT)
         else:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+            return Response({'message': 'requested image already deleted/unavailable'},status=status.HTTP_404_NOT_FOUND)
 
     def patch(self,request, img_name):
-        '''This method will replaces teh image with image contained in the body of the request'''
+        '''This method will replaces the image with image contained in the body of the request'''
         user = request.user.username
         image_exists = 'img-file' in request.FILES
         path = os.listdir(os.path.join(settings.MEDIA_ROOT,user, 'photos'))
@@ -46,7 +42,6 @@ class Oneimage(APIView):
             os.remove(img_name)
         else:
             return Response({'message':'Image not found'},status=status.HTTP_404_NOT_FOUND)
-
 
         if image_exists:
             img_file = request.FILES['img-file']
@@ -90,32 +85,17 @@ class Imagesall(APIView):
             return Response({'message': 'Image not present in request body'}, status=status.HTTP_400_BAD_REQUEST)
 
 
-class UserLoginView(View):
-    form_class = Loginform
-    template_name = 'login.html'
-
-    def get(self,request):
-        form = self.form_class(None)
-        return render(request,self.template_name,{'form':form})
-
-    def post(self,request):
-        form = self.form_class(request.POST)
-        user = authenticate(username=request.POST['username'],password=request.POST['password'])
-        if user is not None:
-            if user.is_active:
-                login(request,user)
-                return redirect('image_store:list')
-        return render(request, self.template_name, {'form': form})
-
-
-
 def optimize(request):
-    '''Code to optimze png images'''
+    '''Code to optimize images'''
     user = request.user.username
     if 'img-file' in request.FILES:
         img_file = request.FILES['img-file']
         ext = img_file.name.split('.')[1]
-        if ext == 'png':
+        if ext == 'png' or ext == 'bmp' or ext == 'gif':
             os.chdir(os.path.join(settings.MEDIA_ROOT, user, 'photos'))
             com = "optipng -o2 -strip all " + img_file.name
+            os.system(com)
+        elif ext == 'jpg' or ext == 'jpeg':
+            os.chdir(os.path.join(settings.MEDIA_ROOT, user, 'photos'))
+            com = "jpegoptim -f --m=70 "+ img_file.name
             os.system(com)
